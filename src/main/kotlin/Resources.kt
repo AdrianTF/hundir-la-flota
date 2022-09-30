a76960272dae8c0b43e942385dce4982c6c098d0
Mario <mario.espasa.2003@gmail.com>
2022-09-30 11:08:33 +0200

    Refactor data class

--- src/main/kotlin/Resources.kt
+++ src/main/kotlin/Resources.kt
@@ -9,4 +9,46 @@ data class Resources(
     var playerGrid: Array<Array<String?>> = Array(numRows) { arrayOfNulls<String>(numCols) }
     var computerGrid: Array<Array<String?>> = Array(numRows) { arrayOfNulls<String>(numCols) }
     val missedGuesses: Array<Array<Int?>> = Array(numRows) { arrayOfNulls<Int>(numCols) }
+
+    fun crearOceano(){
+        //Linea caracteres oceano
+        print(" | ")
+        for(i in 0..numCols-1){
+            print("  $i   ")
+        }
+        println()
+
+        //Primera seccion del oceano
+        for (i in 0..playerGrid.size-1){
+            for (j in 0..playerGrid[i].size-1){
+                playerGrid[i][j] = "   ~  "
+                computerGrid[i][j] = "   ~  "
+                if(j==0){
+                    print("$i|${playerGrid[i][j]}")
+                }else if(j == playerGrid[i].size-1){
+                    print("${playerGrid[i][j]}|$i")
+                }else{
+                    print("${playerGrid[i][j]}")
+                }
+
+            }
+            println("")
+        }
+
+        //Segunda parte oceano
+        print(" | ")
+        for(i in 0..numCols-1){
+            print("  $i   ")
+        }
+        println("")
+
+
+    }
+
+    fun barcosJugador(){
+
+        println("Coloca tus barcos:")
+
+
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,43 +7,9 @@ fun main() {
     println("Bienvenido al juego de hundir la flota")
     println("  ")
 
-    //Linea caracteres oceano
-    print(" | ")
-    for(i in 0..rs.numCols-1){
-        print("  $i   ")
-    }
-    println()
-
-    //Primera seccion del oceano
-    for (i in 0..rs.playerGrid.size-1){
-        for (j in 0..rs.playerGrid[i].size-1){
-            rs.playerGrid[i][j] = "   ~  "
-            rs.computerGrid[i][j] = "   ~  "
-            if(j==0){
-                print("$i|${rs.playerGrid[i][j]}")
-            }else if(j == rs.playerGrid[i].size-1){
-                print("${rs.playerGrid[i][j]}|$i")
-            }else{
-                print("${rs.playerGrid[i][j]}")
-            }
-
-        }
-        println("")
-    }
-
-    //Segunda parte oceano
-    print(" | ")
-    for(i in 0..rs.numCols-1){
-        print("  $i   ")
-    }
-    println("")
-    barcosJugador(rs.numCols,rs.playerGrid,rs.computerGrid,rs.playerShips)
+    rs.crearOceano()
+    rs.barcosJugador()
 
 }
 
-fun barcosJugador(nCols: Int,pGrid:Array<Array<String?>>,cGrid:Array<Array<String?>>,pShips:Int){
 
-    println("Coloca tus barcos:")
-
-
-}
